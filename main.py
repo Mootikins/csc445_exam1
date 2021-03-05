5413055b93dbe63875b46c18287c23239c12ecb2
Matthew Krohn <matthew.d.krohn@gmail.com>
2021-03-05 05:57:49 -0700

    New arg for output file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from networkx.classes.graph import Graph
 
 def make_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="%(prog)s action file",
+        usage="%(prog)s action input_file [options]",
         description="Convert an NFA to a DFA, minimize a DFA, or both in"
         " sequence. Writes the output .dot file to 'grid.dot' and a PDF rendering"
         " to 'grid.dot.pdf'",
@@ -18,7 +18,13 @@ def make_parser() -> argparse.ArgumentParser:
         help="convert an NFA to DFA, minimize a DFA, or both",
         choices=("convert", "minimize", "both"),
     )
-    parser.add_argument("file")
+    parser.add_argument("input_file", help="Input filename in graphviz .gv format")
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        help="Output filename for intermediate .dot file and the PDF. Defaults to 'grid.dot' and 'grid.dot.pdf'",
+        default="grid.dot",
+    )
     return parser
 
 
@@ -35,29 +41,33 @@ def remove_inaccessible(G: Graph, initial: Any = "qi"):
                 G.remove_node(node)
 
 
-def output_graph(G: Graph):
+def output_graph(G: Graph, output: str):
     for (start, end, data) in G.edges(data=True):
         if data.get("label") is not None and type(data.get("label")) is list:
             G[start][end]["label"] = ",".join(data.get("label"))
 
-    nx.drawing.nx_agraph.write_dot(G, "grid.dot")
-    gv = graphviz.Source.from_file("grid.dot")
-    graphviz.render("dot", "pdf", "grid.dot")
+    nx.drawing.nx_agraph.write_dot(G, output)
+    graphviz.render("dot", "pdf", output)
 
 
 def convert(G: Graph):
     pass
 
 
-def minimize(G: Graph):
+def mark(G: Graph):
     pass
+
+
+def minimize(G: Graph):
+    remove_inaccessible(G)
 
 
 def main():
     parser = make_parser()
     args = parser.parse_args()
 
-    G = nx.DiGraph(nx.drawing.nx_agraph.read_dot(args.file))
+    G = nx.DiGraph(nx.drawing.nx_agraph.read_dot(args.input_file))
+    attrs_to_lists(G)
 
     if args.action in ["convert", "both"]:
         convert(G)
@@ -65,7 +75,7 @@ def main():
     if args.action in ["minimize", "both"]:
         minimize(G)
 
-    output_graph(G)
+    output_graph(G, args.output_file)
 
 
 if __name__ == "__main__":
